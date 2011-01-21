859dd57c971317f4393253a490896a731ca17b0c
Jaime Oyarzún Knittel <joyarzun@inf.utfsm.cl>
2011-01-21 13:05:31 -0300

    Added accessor to passes for events.
    Signed-off-by: Jaime Oyarzún Knittel <joyarzun@inf.utfsm.cl>

--- lib/welcu/client/event.rb
+++ lib/welcu/client/event.rb
@@ -13,16 +13,19 @@ module Welcu
   
   class Event < Base
     attr_accessor :company
+    attr_accessor :passes
     
     attribute :name, :short_name, :permalink, :description, :starts_at, :ends_at, :locale, :logo, :location, :url, :logo_big
     
     def load!
       @attributes = client.get('event')['event']
       self.company = Welcu::Company.new(self, @attributes.delete('company'))
+      self.passes = client.passes
       self.company.event = self
       client.locale ||= self.locale
       self
     end
+
   end
   
   class Company < Base
